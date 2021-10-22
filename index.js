bb3e23abb545a5ce965cf30a8b3a6f79489160be
Gian López <lopezarizagianlucas@gmail.com>
2021-10-22 02:44:50 -0500

    masin: the icon fonts component was added.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 import { name } from './app.json';
+import { FontIcon } from './src/assets/icons';
 import { BurgerMenu } from './src/shared/components/BurgerMenu';
 
 function App() {
   return (
     <View style={{ padding: 15 }}>
-      <BurgerMenu/>
+      <BurgerMenu/>      
     </View>
   )
 };
---
+++ src/assets/icons/index.js
@@ -0,0 +1,8 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import icomoonConfig from './config.json';
+
+export const FontIcon = createIconSetFromIcoMoon(
+  icomoonConfig,
+  'ChaseIcons',
+  'chase-icons.ttf'
+)
